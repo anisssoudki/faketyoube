49156c7771275395f539d70c8d7f5b8a36b67b8c
Aniss <59289660+anisssoudki@users.noreply.github.com>
2020-06-01 11:23:12 -0700

    Update comments_controller.rb

--- Faketyoub/app/controllers/comments_controller.rb
+++ Faketyoub/app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController 
- post '/comments' do 
-      if 
-         logged_in?
-         comments = Comment.create(content: params[:content], date: params[Date.today], user_id:  params[:user_id],video_id: params[:video_id])
-         redirect '/videos'
+    post '/comments' do 
+       if 
+           logged_in? 
+            time = Time.new
+            if params[:content] !=""
+                comments = Comment.create(content: params[:content], date: time   , user_id:  params[:user_id],video_id: params[:video_id])
+             end
+        redirect '/videos'
+    
        else 
-         redirect '/signup'
+        redirect '/signup'
        end
-    end
+    end 
 end
